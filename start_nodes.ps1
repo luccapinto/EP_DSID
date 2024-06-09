929df0cd9103465ebf866f2fd8861436c4ba40cf
Lucca Rodrigues Assunção Pinto <tripaca123@gmail.com>
2024-06-09 05:16:33 -0300

    modificando inicializacao em lote

--- start_nodes.ps1
+++ start_nodes.ps1
@@ -1,3 +1,3 @@
-Start-Process "cmd" -ArgumentList "/k python node.py 127.0.0.1:5001 topologia_linha/1.txt key_values/key_value1.txt"
-Start-Process "cmd" -ArgumentList "/k python node.py 127.0.0.1:5002 topologia_linha/2.txt key_values/key_value2.txt"
-Start-Process "cmd" -ArgumentList "/k python node.py 127.0.0.1:5003 topologia_linha/3.txt key_values/key_value3.txt"
+Start-Process "cmd" -ArgumentList "/k python node.py 127.0.0.1:5001 topologia_ciclo_3/1.txt key_values/key_value1.txt"
+Start-Process "cmd" -ArgumentList "/k python node.py 127.0.0.1:5002 topologia_ciclo_3/2.txt key_values/key_value2.txt"
+Start-Process "cmd" -ArgumentList "/k python node.py 127.0.0.1:5003 topologia_ciclo_3/3.txt key_values/key_value3.txt"
